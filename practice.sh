1e1a764f717f6bc1acf2aac8f48c1b8de0ed31ef
learncrackdevops <ysaibhavyateja@gmail.com>
2023-11-09 17:22:15 +0530

    git

--- practice.sh
+++ practice.sh
@@ -1,2 +1,11 @@
-git bash
-echo -e "hellow world"
+dnf install nginx -y
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
+
+
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+cp expenses.conf /etc/nginx/default.d/expense.conf
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+systemctl restart nginx
